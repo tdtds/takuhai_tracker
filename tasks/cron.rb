9fe1fc02f91f8d6e5e60466923baa9b285e4d6d1
TADA Tadashi <t@tdtds.jp>
2015-07-08 16:17:25 +0900

    modify debug message

--- tasks/cron.rb
+++ tasks/cron.rb
@@ -14,15 +14,15 @@ task :cron do
 	TakuhaiTracker::Item.all.each do |item|
 		info "start checking #{item.key}"
 		if item.service
-			info "   found existent item of #{item.service}"
+			info "   => found existent item of #{item.service}"
 			item_new = TakuhaiStatus.const_get(item.service).new(item.key)
 		else
-			info "   found unhandled item"
+			info "   => found unhandled item"
 			item_new = TakuhaiStatus.scan(item.key) rescue nil
 		end
 
 		if item_new && item.state != item_new.state
-			info "   start state checking"
+			info "   => start state checking"
 			setting = TakuhaiTracker::Setting.where(user_id: item.user_id).first
 			if setting
 				Pushbullet.api_token = setting.pushbullet
@@ -34,11 +34,11 @@ task :cron do
 
 			item.update_attributes!(time: item_new.time, state: item_new.state)
 		else
-			info "   not updated"
+			info "   => not updated"
 		end
 
 		if item_new && item_new.finish?
-			info "   remove item because finished."
+			info "   => remove item because finished."
 			item.remove
 		end
 	end
